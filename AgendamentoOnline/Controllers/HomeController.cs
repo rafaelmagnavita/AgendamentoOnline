4b55cf4d1440573ad8539839f37225210c148fa9
Rafael Magnavita <rafaelmagnavita@hotmail.com>
2023-06-15 21:43:40 -0300

    ok

--- AgendamentoOnline/Controllers/HomeController.cs
+++ AgendamentoOnline/Controllers/HomeController.cs
@@ -1,17 +1,77 @@
-﻿using System;
+﻿using AgendamentoOnline.Models;
+using AgendamentoOnline.Utils;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AgendamentoOnline.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ScheduleContext _context;
+
+        public HomeController(ScheduleContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            try
+            {
+                var userExists = _context.Users.Where(a => a.Login == user.Login).FirstOrDefault();
+                if (userExists != null && user.Password.Equals(userExists?.Password))
+                {
+                    Session["usuario"] = userExists;
+                    var ticket = FormsAuthentication.Encrypt(new FormsAuthenticationTicket(
+                    1, userExists.Name, DateTime.Now, DateTime.Now.AddHours(12), true, userExists.Type.ToString()));
+                    var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, ticket);
+                    Response.Cookies.Add(cookie);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Login ou Senha Incorretos");
+                return View();
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogErros("Erro em Login-Post: " + ex.Message);
+                throw;
+            }
+        }
+        [Authorize]
+        public ActionResult LogOut()
+        {
+            try
+            {
+                Session.Clear();
+                Session.Abandon();
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Login", "Home");
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogErros("LogOut: " + ex.Message);
+                throw;
+            }
+
+        }
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
+        [Authorize]
 
         public ActionResult About()
         {
@@ -19,6 +79,7 @@ namespace AgendamentoOnline.Controllers
 
             return View();
         }
+        [Authorize]
 
         public ActionResult Contact()
         {
---
+++ AgendamentoOnline/Utils/LogManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace AgendamentoOnline.Utils
+{
+    public class LogManager
+    {
+        /// <summary>
+        /// Função para gerar relatorio de erros do programa nos logs do windows
+        /// </summary>
+        /// <param name="erro"></param>
+        public static void LogErros(string erro)
+        {
+            try
+            {
+                if (!EventLog.SourceExists("AgendamentoOnline"))
+                {
+                    EventLog.CreateEventSource("AgendamentoOnline", "AgendamentoOnline - ErrorReport");
+                }
+                EventLog myLog = new EventLog();
+                myLog.Source = "AgendamentoOnline";
+                myLog.WriteEntry(erro);
+            }
+            catch (Exception exp)
+            {
+            }
+
+        }
+    }
+}
