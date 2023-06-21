5afba17b616ae301891d4d8e7ad5e69f6578b8a1
Rafael Magnavita <rafaelmagnavita@hotmail.com>
2023-06-21 17:31:45 -0300

    functions to encrypt password

---
+++ AgendamentoOnline/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgendamentoOnline.Models;
+using AgendamentoOnline.Utils;
+using AgendamentoOnline.Utils.Enums;
+
+namespace AgendamentoOnline.Controllers
+{
+    [Filters.SessionExpiredCheck]
+    [Authorize]
+    public class UserController : Controller
+    {
+        #region Variables
+        private ScheduleContext _context = new ScheduleContext();
+
+        #endregion
+
+        #region Controllers
+        public ActionResult Index(List<User> listUser = null)
+        {
+            try
+            {
+                if (listUser == null)
+                {
+                    listUser = new List<User>();
+                    User user = Session["user"] as User;
+                    listUser = _context.Users.OrderBy(a => a.Name).ToList();
+
+                    if (!(user is AdminUser))
+                    {
+                        listUser = listUser.Where(a => a.Type == (int)UserType.PATIENT).ToList();
+
+                    }
+                }
+                foreach (User user in listUser)
+                {
+                }
+                return View(listUser);
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogErros("Erro em Login-Get: " + ex.Message);
+                throw;
+            }
+        }
+
+        public ActionResult IndexPat()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogErros("Erro em Login-Get: " + ex.Message);
+                throw;
+            }
+            return View();
+        }
+
+        public ActionResult IndexAdm()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogErros("Erro em Login-Get: " + ex.Message);
+                throw;
+            }
+            return View();
+        }
+
+        #endregion
+
+        #region Methods
+        #endregion
+
+
+    }
+}
---
+++ AgendamentoOnline/Utils/Security.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AgendamentoOnline.Utils
+{
+    public class Security
+    {
+        public static class PasswordEncryption
+        {
+            private static readonly string Key = "t^g[p|j#UO(4e.)k";
+
+            public static string EncryptPassword(string password)
+            {
+                byte[] keyBytes = Encoding.UTF8.GetBytes(Key);
+
+                using (var aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = keyBytes;
+                    aesAlg.IV = new byte[16];
+
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (var swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(password);
+                            }
+
+                            byte[] encryptedBytes = msEncrypt.ToArray();
+                            string encryptedPassword = Convert.ToBase64String(encryptedBytes);
+
+                            return encryptedPassword;
+                        }
+                    }
+                }
+            }
+
+            public static string DecryptPassword(string encryptedPassword)
+            {
+                byte[] keyBytes = Encoding.UTF8.GetBytes(Key);
+                byte[] encryptedBytes = Convert.FromBase64String(encryptedPassword);
+
+                using (var aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = keyBytes;
+                    aesAlg.IV = new byte[16];
+
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                    using (var msDecrypt = new MemoryStream(encryptedBytes))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                string decryptedPassword = srDecrypt.ReadToEnd();
+                                return decryptedPassword;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
