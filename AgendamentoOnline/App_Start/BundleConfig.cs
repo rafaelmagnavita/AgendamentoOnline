f6b433ddf9b882237526b941e9094c04e72c1d57
Rafael Magnavita <rafaelmagnavita@hotmail.com>
2023-06-24 23:05:38 -0300

    dataTable

--- AgendamentoOnline/App_Start/BundleConfig.cs
+++ AgendamentoOnline/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace AgendamentoOnline
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            // Add DataTables scripts to the bundle
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/Scripts/DataTables/datatables.min.js"));
+
             // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
             // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -24,7 +28,13 @@ namespace AgendamentoOnline
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Scripts/DataTables/datatables.min.css")); // Include DataTables CSS
+
+            // Include the DataTables bundle in the Layout or View where you need it
+            // Example: @Scripts.Render("~/bundles/datatables")
+
         }
+
     }
 }
